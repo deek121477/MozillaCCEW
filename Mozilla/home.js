79b937ace7415145a90abb000409a8925fe0e5d6
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2022-01-18 15:59:22 +0530

    Add files via upload

---
+++ Mozilla/home.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: [ "Developer", "Student", "CODER", "Dreamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: [ "Developer", "Student", "CODER", "Dreamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
